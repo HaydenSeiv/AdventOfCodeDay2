eb05d250fb0f8dd4d0d84ce1b5c345c618216c85
HaydenSiev <149506008+HaydenSiev@users.noreply.github.com>
2025-07-31 13:48:53 -0600

    Solved part 2

--- AdventOfCodeDay2/Program.cs
+++ AdventOfCodeDay2/Program.cs
@@ -9,7 +9,11 @@ namespace AdventOfCodeDay2
 {
     internal class Program
     {
-
+        /// <summary>
+        /// Method to check if a report is safe. Must be either ascending or descending, and the difference between any two adjacent numbers must be less than or equal to 3.
+        /// </summary>
+        /// <param name="report"></param>
+        /// <returns> True or False </returns>
         public static bool isSafe(int[] report)
         {
             //check if the report is in ascending or descending order
@@ -46,6 +50,39 @@ namespace AdventOfCodeDay2
             //if we get here, the report is safe
             return true;
         }
+
+        /// <summary>
+        /// Method to check if a report is safe with a dampener. A dampener allows us to remove one number from the report and check if it is safe.
+        /// </summary>
+        /// <param name="report"></param>
+        /// <returns> True or False</returns>
+        public static bool isSafeDampener(int[] report) 
+        {
+            //if the report is safe, it is also safe with the dampener
+            if (isSafe(report))
+            {
+                return true;
+            }
+
+            //if not, remove a number and check again until all numbers have been removed
+            for (int i = 0; i < report.Length; i++)
+            {
+                //create a new array without the current number
+                int[] newReport = report.Where((val, index) => index != i).ToArray();
+                //check if the new report is safe
+                if (isSafe(newReport))
+                {
+                    return true;
+                }
+            }
+            //if we reach here, report is not safe even with a dampener
+            return false;
+        }
+        /// <summary>
+        /// Method to read the file and turn each line into an int array.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public static List<int[]> getReports(string path)
         {
             List<int[]> reports = new List<int[]>();
@@ -66,19 +103,21 @@ namespace AdventOfCodeDay2
             }
             return reports;
         }
-        static void Main(string[] args)
-        {
-            //count to track number of safe reports
-            int safeCount = 0;
 
-            //turn data into list of "reports"
-            List<int[]> reports = getReports("../../data.txt");
+        /// <summary>
+        /// Method to count the number of safe reports.
+        /// </summary>
+        /// <param name="reports"></param>
+        /// <returns></returns>
+        public static int safeCount(List<int[]> reports)
+        {
+            int safeCount = 0;
 
             //go through reports and check if they are safe
             foreach (int[] report in reports)
             {
                 //check if the report is safe
-                bool safe = isSafe(report);
+                bool safe = isSafeDampener(report);
                 //if the report is safe, increment the count
                 if (safe)
                 {
@@ -86,8 +125,16 @@ namespace AdventOfCodeDay2
                 }
             }
 
+            return safeCount;
+
+        }
+        static void Main(string[] args)
+        {
+            //turn data into list of "reports"
+            List<int[]> reports = getReports("../../data.txt");
+
             //print the number of safe reports
-            Console.WriteLine($"Number of safe reports: {safeCount}");
+            Console.WriteLine($"Number of safe reports: {safeCount(reports)}");
         }
     }
 }
